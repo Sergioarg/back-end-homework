2c92cce6d172e9dcb99d912a73789117e390063e
sergioarg <sergio_ramos53@yahoo.com>
2024-05-31 20:47:15 -0500

    Update test using status

--- users/tests/test_users.py
+++ users/tests/test_users.py
@@ -1,6 +1,8 @@
 """ Test Users endpoints  """
-from rest_framework.test import APITestCase
 from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 class UsersTests(APITestCase):
     """ Test Users endpoints """
@@ -16,21 +18,21 @@ class UsersTests(APITestCase):
         """ Test Create new user """
         response = self.client.post(self.users_url, self.user_body)
 
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_create_user_without_email(self):
         """ Test Create new user without email """
         del self.user_body['email']
         response = self.client.post(self.users_url, self.user_body)
 
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_user_without_password(self):
         """ Test Create new user without password """
         del self.user_body['password']
         response = self.client.post(self.users_url, self.user_body)
 
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_user_same_email(self):
         """ Test Create new user with same email """
@@ -41,39 +43,39 @@ class UsersTests(APITestCase):
         # Create second user with same email
         response = self.client.post(self.users_url, self.user_body)
 
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_user_with_invalid_email(self):
         """ Test Create new user with invalid email """
         self.user_body['email'] = 'test'
         response = self.client.post(self.users_url, self.user_body)
 
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_with_less_than_ten_characters(self):
         """ Test Create new user with less than 10 characters """
         self.user_body['password'] = 'TestP@ss'
         response = self.client.post(self.users_url, self.user_body)
 
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_without_one_lowercase_character(self):
         """ Test Create new user without one lowercase character """
         self.user_body['password'] = 'TESTP@SSWORD'
         response = self.client.post(self.users_url, self.user_body)
 
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_without_one_uppercase_character(self):
         """ Test Create new user without one uppercase character """
         self.user_body['password'] = 'testp@ssword'
         response = self.client.post(self.users_url, self.user_body)
 
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_without_one_special_character(self):
         """ Test Create new user without one special character """
         self.user_body['password'] = 'TestPassword'
         response = self.client.post(self.users_url, self.user_body)
 
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
