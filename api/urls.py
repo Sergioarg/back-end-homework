2536b999f7cb31f9902c320a2a5526f8a1fbb8f6
sergioarg <sergio_ramos53@yahoo.com>
2024-06-02 11:17:43 -0500

    Updateing README with info of users endpoint

--- api/urls.py
+++ api/urls.py
@@ -9,15 +9,15 @@ from movies.views import MoviesViewSet, GenresViewSet
 
 # Rest endpoints
 router = DefaultRouter()
-router.register(r'genres', GenresViewSet, basename='genres')
-router.register(r'movies', MoviesViewSet, basename='movies')
+router.register(r'api/genres', GenresViewSet, basename='genres')
+router.register(r'api/movies', MoviesViewSet, basename='movies')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('random/', RandomNumberView.as_view(), name='random-number'),
     # Users
-    path('users/register/', CreateUserView.as_view(), name='create-user'),
-    path('users/login/', LoginView.as_view(), name='login-user'),
+    path('api/users/register/', CreateUserView.as_view(), name='create-user'),
+    path('api/users/login/', LoginView.as_view(), name='login-user'),
 
     path('admin/', admin.site.urls),
 
